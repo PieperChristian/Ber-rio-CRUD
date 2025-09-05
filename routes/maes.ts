7cb3ef21cafca378c53052c80652d3fb217c7429
PieperChristian <cvilelapieper@gmail.com>
2025-09-05 11:55:29 -0300

    cria a estrutura básica do CRUD

--- routes/maes.ts
+++ routes/maes.ts
@@ -6,4 +6,40 @@ const prisma = new PrismaClient({
     log: ["query"]
 })
 
+router.get("/", async (req, res) => {
+    try {
+        
+        res.status(200).json()
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+})
+
+router.post("/", async (req, res) => {
+    try {
+        
+        res.status(200).json()
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+})
+
+router.put("/:id", async (req, res) => {
+    try {
+        
+        res.status(200).json()
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+})
+
+router.delete("/:id", async (req, res) => {
+    try {
+        
+        res.status(200).json()
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+})
+
 export default router
