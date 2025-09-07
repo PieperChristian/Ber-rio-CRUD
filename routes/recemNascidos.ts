0ebcee00d390c5af321d9723203a591217887156
PieperChristian <cvilelapieper@gmail.com>
2025-09-07 16:58:57 -0300

    cria o schema de validacao para recemNascidos e adiciona campo sexo ao squema.prisma_recemNascidos

--- routes/recemNascidos.ts
+++ routes/recemNascidos.ts
@@ -1,9 +1,21 @@
 import { Router } from "express"
 import { PrismaClient } from "@prisma/client"
+import { z } from "zod"
 
 const router = Router()
 const prisma = new PrismaClient({
     log: ["query"]
+})
+
+import { Parto } from "@prisma/client"
+
+const rnValidator = z.object({
+    nome: z.string().min(10, "Nome deve ter no mínimo 10 caracteres"),
+    dataNasc: z.string().datetime("Data deve ser no formato aaaa-mm-ddT00:00:00Z"),
+    peso: z.number().min(1000, "Peso deve ser no mínimo 1000g").max(10000, "Peso deve ser no máximo 10000g"),
+    parto: z.nativeEnum(Parto).optional(),
+    altura: z.number().min(10, "Altura deve ser no mínimo 10cm").max(1000, "Altura deve ser no máximo 1000cm"),
+    sexo: z.enum(["M", "F"], "Sexo deve ser 'M' ou 'F'")
 })
 
 router.get("/", async (req, res) => {
