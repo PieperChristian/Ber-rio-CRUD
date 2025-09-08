1f4821b5cc6a72b9fc65533eada31a07e6b1129d
PieperChristian <cvilelapieper@gmail.com>
2025-09-08 17:30:28 -0300

    define a logica da router put de recemNascido.ts

--- routes/recemNascidos.ts
+++ routes/recemNascidos.ts
@@ -10,12 +10,12 @@ const prisma = new PrismaClient({
 import { Parto } from "@prisma/client"
 import { Sexo } from "@prisma/client"
 
-const rnValidator = z.object({
+const rnValidatocao = z.object({
     nome_: z.string().min(10, "Nome deve ter no mínimo 10 caracteres"),
     dataNasc_: z.string().datetime("Data deve ser no formato aaaa-mm-ddT00:00:00Z"),
     peso_: z.number().min(1000, "Peso deve ser no mínimo 1000g").max(10000, "Peso deve ser no máximo 10000g"),
     parto_: z.nativeEnum(Parto),
-    altura_: z.number().min(10, "Altura deve ser no mínimo 10cm").max(1000, "Altura deve ser no máximo 1000cm"),
+    altura_: z.number().min(10, "Altura deve ser no mínimo 10cm").max(100, "Altura deve ser no máximo 100cm"),
     sexo_: z.nativeEnum(Sexo)
 })
 
@@ -31,14 +31,14 @@ router.get("/", async (req, res) => {
 })
 
 router.post("/", async (req, res) => {
-    const recemNascido = rnValidator.safeParse(req.body)
+    const recemNascido = rnValidatocao.safeParse(req.body)
 
     if (!recemNascido.success) {
         return res.status(400).json(recemNascido.error.format())
     }
+
     try {
         const novoRecemNascido = await prisma.recem_Nascido.create({
-            // const { nome, dataNasc, peso, parto, altura, sexo, mae, medico } = recemNascido.data
             data: {
                 nome: recemNascido.data.nome_,
                 dataNasc: recemNascido.data.dataNasc_,
@@ -50,15 +50,39 @@ router.post("/", async (req, res) => {
                 medico: req.body.medico
             }
         })
-        res.status(200).json(novoRecemNascido)
+        res.status(200).json({ message: "Recem nascido cadastrado com sucesso", novoRecemNascido })
     } catch (error) {
         res.status(500).json({ error: error })
     }
 })
 
 router.put("/:id", async (req, res) => {
+    const { id } = req.params
+
+    if (Number.isNaN(Number(id))) {
+        return res.status(400).json({ error: "Código Inváido."})
+    }
+
+    const recemNascido = rnValidatocao.safeParse(req.body)
+
+    if (!recemNascido.success) {
+        return res.status(400).json(recemNascido.error.format())
+    }
+
     try {
-        
+        const atualizaRecemNascido = await prisma.recem_Nascido.update({
+            where: { id: Number(id)},
+            data: {
+                nome: recemNascido.data.nome_,
+                dataNasc: recemNascido.data.dataNasc_,
+                peso: recemNascido.data.peso_,
+                parto: recemNascido.data.parto_,
+                altura: recemNascido.data.altura_,
+                sexo: recemNascido.data.sexo_,
+                mae: req.body.mae,
+                medico: req.body.medico
+            }
+        })
         res.status(200).json()
     } catch (error) {
         res.status(500).json({ error: error })
