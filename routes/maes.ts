53de1fbbc653b8b909e12da4ebaabc0c6e80d6ed
PieperChristian <cvilelapieper@gmail.com>
2025-09-09 17:45:01 -0300

    cria a router post para mae

--- routes/maes.ts
+++ routes/maes.ts
@@ -7,6 +7,13 @@ const prisma = new PrismaClient({
     log: ["query"]
 })
 
+const maeValidacao = z.object({
+    nome: z.string().min(10, "Nome deve ter no mínimo 10 caracteres"),
+    endereco: z.string(),
+    telefone:z.string().length(11, "Telefone deve ter 11 digitos com o DDD"),
+    dataNasc:z.string().datetime("Data deve ser no formato aaaa-mm-ddT00:00:00Z")
+})
+
 router.get("/", async (req, res) => {
     try {
         
@@ -17,8 +24,21 @@ router.get("/", async (req, res) => {
 })
 
 router.post("/", async (req, res) => {
+    const mae = maeValidacao.safeParse(req.body)
+    if (!mae.success) {
+        return res.status(400).json
+    }
+
     try {
-        res.status(200).json()
+        const novaMae = await prisma.mae.create({
+            data: {
+                nome: mae.data.nome,
+                endereco: mae.data.endereco,
+                telefone: mae.data.telefone,
+                dataNasc: mae.data.dataNasc
+            }
+        })
+        res.status(200).json({ message: "Mae cadastrada com sucesso", novaMae })
     } catch (error) {
         res.status(500).json({ error: error })
     }
