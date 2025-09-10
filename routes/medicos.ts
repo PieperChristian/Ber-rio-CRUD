69a58577ef7ff864d0d4802733817a2a9e6247c2
PieperChristian <cvilelapieper@gmail.com>
2025-09-10 17:49:17 -0300

    cria a logica de validacao com zod para medico.ts

--- routes/medicos.ts
+++ routes/medicos.ts
@@ -1,9 +1,19 @@
 import { Router } from "express"
 import { PrismaClient } from "@prisma/client"
+import z from "zod"
 
 const router = Router()
 const prisma = new PrismaClient({
     log: ["query"]
+})
+
+import { Especialidade } from "@prisma/client"
+
+const medicoValidacao = z.object({
+    crm: z.string().length(8, "CRM deve ter 8 digitos"),
+    nome: z.string().min(10, "Nome deve ter no mínimo 10 caracteres"),
+    telefone: z.string().length(11, "Telefone deve ter 11 digitos com o DDD"),
+    especialidade: z.nativeEnum(Especialidade)
 })
 
 router.get("/", async (req, res) => {
